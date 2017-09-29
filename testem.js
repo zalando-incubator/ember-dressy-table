0406d6d68d43e86e0a8ce96ef1c5ac0d05396e98
chilicoder <chilicoder@users.noreply.github.com>
2017-09-29 17:13:19 +0200

    fixing testem

--- testem.js
+++ testem.js
@@ -8,5 +8,15 @@ module.exports = {
   "launch_in_dev": [
     "PhantomJS",
     "Chrome"
-  ]
+  ],
+  "browser_args": {
+    "Chrome": [
+      "--headless",
+      "--disable-gpu",
+      "--remote-debugging-port=9222",
+      "--remote-debugging-address=0.0.0.0",
+      "--no-sandbox",
+      "--user-data-dir=/tmp"
+    ]
+  }
 };
